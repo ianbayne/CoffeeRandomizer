4255bfcd4c0882cd4ab967db1cbf148de54b44e9
Ian Bayne <ian@tacchistudios.com>
2020-03-20 10:11:32 +0900

    new: add constants for brewing

---
+++ src/constants.js
@@ -0,0 +1,5 @@
+export const STIRRING = {};
+export const WATER_TEMPERATURE = {};
+export const COFFEE_TO_WATER_RATIO = {};
+export const GRIND_AND_BREW_TIME = {};
+export const BLOOM_TIME_AND_INVERSION = {};
