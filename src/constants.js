2cf8c1b8485b192f16e6deccd07691ef2ba43ca1
Ian Bayne <ian@tacchistudios.com>
2020-03-20 22:44:55 +0900

    new: update constants with dice values

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,33 @@
-export const STIRRING = {};
-export const WATER_TEMPERATURE = {};
-export const COFFEE_TO_WATER_RATIO = {};
-export const GRIND_AND_BREW_TIME = {};
-export const BLOOM_TIME_AND_INVERSION = {};
+export const STIRRING = [
+  'Stir 1 time before pressing',
+  'NSEW stir before pressing',
+  'Stir 2 times in the same direction before pressing',
+  'Stir once in each direction before pressing',
+  "Don't stir",
+  'Your choice',
+];
+export const WATER_TEMPERATURE = [75, 80, 85, 90, 95, 'your choice'];
+export const COFFEE_TO_WATER_RATIO = [
+  '12g coffee to 200g water',
+  '15g coffee to 200g water',
+  '15g coffee to 250g water',
+  '24g coffee to 200g water (Dilute to share)',
+  '30g coffee to 200g water (Dilute to share)',
+  'Your choice',
+];
+export const GRIND_AND_BREW_TIME = [
+  'Very fine, 30s',
+  'Fine, 60s',
+  'Medium fine, 90s',
+  'Medium, 120s',
+  'Coarse, 4min',
+  'Your choice',
+];
+export const BLOOM_TIME_AND_INVERSION = [
+  'Standard orientation, no bloom',
+  'Inverted orientation, no bloom',
+  'Standard orientation, 30s bloom with 30g water',
+  'Standard orientation, 30s bloom with 60g water',
+  'Inverted orientation, 30s bloom with 30g water',
+  'Inverted orientation, 30s bloom with 60g water',
+];
