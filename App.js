94a25ae3874ba7d85fa472e5e580624816bde4cf
Ian Bayne <ian@tacchistudios.com>
2020-03-21 11:12:10 +0900

    new: implement basic functionality

--- App.js
+++ App.js
@@ -6,66 +6,60 @@
  * @flow
  */
 
-import React from 'react';
+import React, {useState} from 'react';
 import {
+  Button,
   SafeAreaView,
   StyleSheet,
   ScrollView,
-  View,
   Text,
+  View,
   StatusBar,
 } from 'react-native';
 
 import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+  STIRRING,
+  WATER_TEMPERATURE,
+  COFFEE_TO_WATER_RATIO,
+  GRIND_AND_BREW_TIME,
+  BLOOM_TIME_AND_INVERSION,
+} from './src/constants';
 
 const App: () => React$Node = () => {
+  const [stirring, setStirring] = useState(null);
+  const [waterTemp, setWaterTemp] = useState(null);
+  const [coffeeToWaterRatio, setCoffeeToWaterRatio] = useState(null);
+  const [grindAndBrewTime, setGrindAndBrewTime] = useState(null);
+  const [bloomTimeAndInversion, setBloomTimeAndInversion] = useState(null);
+
+  function selectRandomItem(items) {
+    return items[Math.floor(Math.random() * items.length)];
+  }
+
+  function onPress() {
+    setStirring(selectRandomItem(STIRRING));
+    setWaterTemp(selectRandomItem(WATER_TEMPERATURE));
+    setCoffeeToWaterRatio(selectRandomItem(COFFEE_TO_WATER_RATIO));
+    setGrindAndBrewTime(selectRandomItem(GRIND_AND_BREW_TIME));
+    setBloomTimeAndInversion(selectRandomItem(BLOOM_TIME_AND_INVERSION));
+  }
+
   return (
     <React.Fragment>
       <StatusBar barStyle="dark-content" />
       <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
+        <ScrollView>
+          <View style={styles.header}>
+            <Text style={styles.headerText}>
+              Welcome to the Aeropress Recipe maker!
+            </Text>
           </View>
+          <Button title="Randomize" onPress={onPress} />
+          <Text>{stirring}</Text>
+          <Text>{waterTemp}</Text>
+          <Text>{coffeeToWaterRatio}</Text>
+          <Text>{grindAndBrewTime}</Text>
+          <Text>{bloomTimeAndInversion}</Text>
         </ScrollView>
       </SafeAreaView>
     </React.Fragment>
@@ -73,41 +67,13 @@ const App: () => React$Node = () => {
 };
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
+  header: {
+    marginTop: 20,
   },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
+  headerText: {
     fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
+    fontWeight: 'bold',
+    textAlign: 'center',
   },
 });
 
