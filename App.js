fc5bfd387b9b958ca1dad7a0c61c7da0f197c84f
Ian Bayne <ian@tacchistudios.com>
2020-03-29 19:20:02 +0900

    change: display text for tab navigation and hide labels

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Text} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
@@ -10,7 +11,26 @@ const Tab = createBottomTabNavigator();
 const App: () => React$Node = () => {
   return (
     <NavigationContainer>
-      <Tab.Navigator initialRouteName="Recipes">
+      <Tab.Navigator
+        initialRouteName="Recipes"
+        screenOptions={({route}) => ({
+          tabBarIcon: ({focused, color, size}) => {
+            let text;
+
+            if (route.name === 'Recipes') {
+              text = 'Recipes';
+            } else if (route.name === 'Settings') {
+              text = 'Settings';
+            }
+
+            return <Text style={{color: color}}>{text}</Text>;
+          },
+        })}
+        tabBarOptions={{
+          activeTintColor: 'tomato',
+          inactiveTintColor: 'gray',
+          showLabel: false,
+        }}>
         <Tab.Screen name="Recipes" component={RecipesScreen} />
         <Tab.Screen name="Settings" component={SettingsScreen} />
       </Tab.Navigator>
