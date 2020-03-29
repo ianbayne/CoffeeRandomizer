b87c5e565ceb83eaff4a3a31021a93f9a478de7b
Ian Bayne <ian@tacchistudios.com>
2020-03-29 21:13:20 +0900

    new: create switches in settings screen

--- src/screens/SettingsScreen.js
+++ src/screens/SettingsScreen.js
@@ -1,19 +1,43 @@
-import React from 'react';
-import {Text, View, StyleSheet} from 'react-native';
+import React, {useState} from 'react';
+import {Text, View, StyleSheet, Switch} from 'react-native';
 
-const SettingsScreen = () => {
+const SettingsScreen = navigation => {
+  const [celsius, setCelsius] = useState(true);
+  function toggleCelsius(value) {
+    setCelsius(value);
+  }
+  const [test, setTest] = useState(false);
+  function toggleTest(value) {
+    setTest(value);
+  }
+
   return (
-    <View style={styles.settings}>
-      <Text>Settings</Text>
+    <View style={styles.switchContainer}>
+      <View style={styles.switchRow}>
+        <Text style={styles.switchText}>Use metric for temperatures</Text>
+        <Switch onValueChange={toggleCelsius} value={celsius} />
+      </View>
+      <View style={styles.switchRow}>
+        <Text style={styles.switchText}>Test test</Text>
+        <Switch onValueChange={toggleTest} value={test} />
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  settings: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+  switchContainer: {
+    marginTop: 100,
+    paddingHorizontal: 30,
+  },
+  switchRow: {
+    flexDirection: 'row',
+    alignItems: 'flex-start',
+    justifyContent: 'space-between',
+    marginVertical: 10,
+  },
+  switchText: {
+    fontSize: 20,
   },
 });
 
