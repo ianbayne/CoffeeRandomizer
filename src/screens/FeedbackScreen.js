ea0e2d8362fbaaba71702609abc9d47da23057e9
Ian Bayne <ian@tacchistudios.com>
2020-05-17 20:17:41 +0900

    new: add webview for feedback screen

--- src/screens/FeedbackScreen.js
+++ src/screens/FeedbackScreen.js
@@ -1,10 +1,37 @@
-import React from 'react';
-import {View, StyleSheet} from 'react-native';
+import React, {useState} from 'react';
+import {View, StyleSheet, ActivityIndicator} from 'react-native';
+import {WebView} from 'react-native-webview';
 
-const FeedbackScreen = ({navigation}) => {
-  return <View />;
+const FeedbackScreen = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  return (
+    <View style={styles.container}>
+      <ActivityIndicator
+        animating={isLoading}
+        size="large"
+        style={styles.activityIndicator}
+      />
+      <WebView
+        onLoad={() => setIsLoading(false)}
+        source={{uri: 'https://infinite.red'}}
+      />
+    </View>
+  );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  activityIndicator: {
+    zIndex: 99,
+    position: 'absolute',
+    top: 0,
+    right: 0,
+    bottom: 0,
+    left: 0,
+  },
+});
 
 export default FeedbackScreen;
