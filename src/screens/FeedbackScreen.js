06af34f59a6c8d868145d86677d971fe959d1bbb
Ian Bayne <ian@tacchistudios.com>
2020-05-30 23:53:11 +0900

    refactor: into single unconcated string

--- src/screens/FeedbackScreen.js
+++ src/screens/FeedbackScreen.js
@@ -5,12 +5,11 @@ import {WebView} from 'react-native-webview';
 const FORM_URI =
   'https://docs.google.com/forms/d/e/1FAIpQLSfezWSiaL5r0zau9iCVie9K4G3tIemrhbQPEi61tlk7OUHd_g/viewform?usp=sf_link';
 
-const INJECTED_JAVASCRIPT =
-  "document.getElementsByClassName('freebirdFormviewerViewNavigationPasswordWarning')[0].remove();" +
-  "document.getElementsByClassName('freebirdFormviewerViewFooterDisclaimer')[0].parentElement.remove();" +
-  "document.getElementsByClassName('freebirdFormviewerViewFeedbackSubmitFeedbackButton')[0].remove();" +
-  "document.getElementsByClassName('freebirdThemedFilledButtonM2')[0].style.backgroundColor = '#ff9900';" +
-  true;
+const INJECTED_JAVASCRIPT = `document.getElementsByClassName('freebirdFormviewerViewNavigationPasswordWarning')[0].remove();
+  document.getElementsByClassName('freebirdFormviewerViewFooterDisclaimer')[0].parentElement.remove();
+  document.getElementsByClassName('freebirdFormviewerViewFeedbackSubmitFeedbackButton')[0].remove();
+  document.getElementsByClassName('freebirdThemedFilledButtonM2')[0].style.backgroundColor = '#ff9900';
+  true;`;
 
 const FeedbackScreen = () => {
   const [isLoading, setIsLoading] = useState(true);
