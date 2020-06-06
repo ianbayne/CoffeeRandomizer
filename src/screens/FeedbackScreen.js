7fac34721de4a47180da90f282755102c632fd4c
Ian Bayne <ian@tacchistudios.com>
2020-06-06 20:14:53 +0900

    change: replace potential Japanese text in feedback form with English

--- src/screens/FeedbackScreen.js
+++ src/screens/FeedbackScreen.js
@@ -5,10 +5,26 @@ import {WebView} from 'react-native-webview';
 const FORM_URI =
   'https://docs.google.com/forms/d/e/1FAIpQLSfezWSiaL5r0zau9iCVie9K4G3tIemrhbQPEi61tlk7OUHd_g/viewform?usp=sf_link';
 
-const INJECTED_JAVASCRIPT = `document.getElementsByClassName('freebirdFormviewerViewNavigationPasswordWarning')[0].remove();
-  document.getElementsByClassName('freebirdFormviewerViewFooterDisclaimer')[0].parentElement.remove();
-  document.getElementsByClassName('freebirdFormviewerViewFeedbackSubmitFeedbackButton')[0].remove();
-  document.getElementsByClassName('freebirdThemedFilledButtonM2')[0].style.backgroundColor = '#ff9900';
+const INJECTED_JAVASCRIPT = `const requiredLegend = document.getElementsByClassName('freebirdFormviewerViewHeaderRequiredLegend')[0]
+  requiredLegend.innerText = '* Required';
+
+  const textInputs = document.getElementsByClassName('quantumWizTextinputPapertextareaPlaceholder');
+  [...textInputs].forEach(element => element.innerText = 'Your answer');
+
+  const passwordWarning = document.getElementsByClassName('freebirdFormviewerViewNavigationPasswordWarning')[0]
+  passwordWarning.remove();
+
+  const footerDisclaimer = document.getElementsByClassName('freebirdFormviewerViewFooterDisclaimer')[0]
+  footerDisclaimer.parentElement.remove();
+
+  const submitFeedbackForFeedbackFormButton = document.getElementsByClassName('freebirdFormviewerViewFeedbackSubmitFeedbackButton')[0]
+  submitFeedbackForFeedbackFormButton.remove();
+
+  const submitButton = document.getElementsByClassName('freebirdThemedFilledButtonM2')[0]
+  submitButton.style.backgroundColor = '#ff9900';
+  
+  const submitButtonText = document.getElementsByClassName('appsMaterialWizButtonPaperbuttonLabel')[0]
+  submitButtonText.innerText = 'Submit';
   true;`;
 
 const FeedbackScreen = () => {
